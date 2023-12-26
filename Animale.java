a165613ca4aa60d5837f797f748cb138700acc76
Rufus97 <manuelgomez@outlook.it>
2023-12-26 17:54:05 +0100

    first commit

---
+++ Animale.java
@@ -0,0 +1,6 @@
+class Gatto extends Animale {
+    @Override
+    void faiIlVerso() {
+        System.out.println("Miagolio del gatto");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Animale animale = new Animale();
+        animale.faiIlVerso();
+        Gatto gatto = new Gatto();
+        gatto.faiIlVerso();
+    }
+}
