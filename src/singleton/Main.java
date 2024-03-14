c51133ac4ff04e47a1117faacb114520f0094628
Rufus97 <manuelgomez@outlook.it>
2024-03-14 14:14:21 +0100

    singleton

--- src/singleton/Main.java
+++ src/singleton/Main.java
@@ -1,13 +1,13 @@
 package singleton;
 
 public class Main {
-	  public static void main(String[] args) {
-	    User user = User.getUser();
-	    System.out.println(user);
-	    User user1 = User.getUser();
-	    user.setName("Ozzy");
-	    user.setSurname("bo");
-	    user.setAge(66);
-	    System.out.println(user1);
-	  }
-	}
+    public static void main(String[] args) {
+        User user = User.getUser();
+        System.out.println(user);
+        User user1 = User.getUser();
+        user.setName("Ozzy");
+        user.setSurname("bo");
+        user.setAge(66);
+        System.out.println(user1);
+    }
+}
